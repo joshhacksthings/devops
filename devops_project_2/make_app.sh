2e027ce891947ee3d4978fbd840cf34ab6ee09ed
joshhacksthings <jdnowak01@gmail.com>
2020-04-25 15:30:08 -0700

    clean up, fix stack deployment

--- devops_project_2/make_app.sh
+++ devops_project_2/make_app.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 aws cloudformation create-stack \
 --stack-name $1 \
---template-body file infrastructure.yml \
---parameters file params.json \
---region=eu-west-1 \
---capabilities CAPABILITY_IAM
+--template-body file://infrastructure.yml \
+--parameters file://params.json \
+--region=us-east-1 \
+--capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM
